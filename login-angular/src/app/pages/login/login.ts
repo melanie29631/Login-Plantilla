53af033a83621587ba4180d01c1eda57b1709f50
melanie29631 <godinezsales21@gmail.com>
2026-01-08 15:11:34 -0600

    login con video

--- login-angular/src/app/pages/login/login.ts
+++ login-angular/src/app/pages/login/login.ts
@@ -20,58 +20,12 @@ export class Login {
     console.log('Password:', this.password);
   }
 
-  // Animación de humo verde
+
   ngAfterViewInit() {
-    this.initSmoke();
+    const video = document.getElementById('bgVideo') as HTMLVideoElement;
+    if (video) {
+      video.playbackRate = 0.5; // 0.5 = la mitad de la velocidad normal
+    }
   }
-
-  initSmoke() {
-    const canvas = document.getElementById('smokeCanvas') as HTMLCanvasElement;
-    const ctx = canvas.getContext('2d')!;
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-
-    let baseHue = 120; // verde
-    const particles: any[] = [];
-
-    function createParticle() {
-      const x = Math.random() * canvas.width;
-      const y = canvas.height + Math.random() * 100; // empieza desde abajo
-      const size = Math.random() * 100 + 50;
-      const speedY = Math.random() * -1.2 - 0.5; // sube lentamente
-      const opacity = Math.random() * 0.3 + 0.2;
-      const hue = baseHue + Math.random() * 40;
-
-      particles.push({ x, y, size, speedY, opacity, hue });
-    }
-
-    function drawParticles() {
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      for (let p of particles) {
-        ctx.beginPath();
-        const gradient = ctx.createRadialGradient(p.x, p.y, 0, p.x, p.y, p.size);
-        gradient.addColorStop(0, `hsla(${p.hue}, 100%, 70%, ${p.opacity})`);
-        gradient.addColorStop(1, `hsla(${p.hue}, 100%, 70%, 0)`);
-        ctx.fillStyle = gradient;
-        ctx.arc(p.x, p.y, p.size, 0, Math.PI * 2);
-        ctx.fill();
-        p.y += p.speedY;
-        p.opacity -= 0.001;
-      }
-      for (let i = particles.length - 1; i >= 0; i--) {
-        if (particles[i].opacity <= 0 || particles[i].y < -100) particles.splice(i, 1);
-      }
-    }
-
-    function animate() {
-      drawParticles();
-      if (particles.length < 250) createParticle();
-      requestAnimationFrame(animate);
-    }
-
-    animate();
-  }
-
-  @HostListener('window:resize') onResize() { const canvas = document.getElementById('smokeCanvas') as HTMLCanvasElement; canvas.width = window.innerWidth; canvas.height = window.innerHeight; }
 
 }
